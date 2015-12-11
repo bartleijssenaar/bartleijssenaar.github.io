787eff7189e27192e131cc87876ecb9d267f1746
bartleijssenaar <bart@butterflyworks.org>
2015-12-11 19:43:31 +0300

    bellt

---
+++ js/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+    $('section#screenshots a').on('click', function() {
+        $('div#modal img').attr('src', $(this).attr('data-image-url') );
+    });
+    
+    var nav = $('.navbar-fixed-top');
+    var distance = $('.navbar-fixed-top').offset();
+    
+    if (distance.top >= 300) {
+        nav.addClass ('effect');
+    }
+    
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        
+        if (scroll >= 300) {
+            nav.addClass('effect');
+        
+        } else {
+            nav.removeClass('effect');
+        
+        }
+    });
+     
+    $('.wp2').waypoint(function() {
+		$('.wp2').addClass('animated fadeInDown');
+	}, {
+		offset: '75%'
+	});
+    
+    $('#about .blue-circle').waypoint (function() {
+        $('#about .blue-circle').addClass('animated fadeInUp')
+    }, {
+        offset: '75%'
+    }) ;
+    
+    $('#services .blue-circle').waypoint (function() {
+        $('#services .blue-circle').addClass('animated fadeInUp')
+    }, {
+        offset: '80%'
+    }) ;
+    
+    $('#screenshots .col-md-3').waypoint (function() {
+        $(this.element).addClass('animated zoomIn')
+        $(this.element).css({'opacity':1});
+    }, {
+        offset: '80%'
+    }) ;
+    
+    
+});
+
+smoothScroll.init( {
+    speed: 3000,
+    easing: 'easeInOutQuad',
+    offset: 50
+
+});
